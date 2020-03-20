4b2f40b753e86b2e2b0c42417a84c0caddfaac9f
srj31 <sourabhrj31@gmail.com>
2020-03-20 21:09:24 +0530

    KMP algo

---
+++ KMP.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> prefix_function(string s){
+	int n = (int)s.length();
+	vector<int> pf(n);
+	pf[0] = 0;
+	for(int i =1;i<n;i++){
+		int j = pf[i-1];
+		while(j>0&&s[i]!=s[j])
+			j = pf[j-1];
+		if(s[i]==s[j])
+			j++;
+		pf[i] = j;
+
+	}
+
+	return pf;
+}
