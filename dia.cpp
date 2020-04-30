371d2dde0a64eaaf54a06fa829b81f5cb1ee4f43
srj31 <sourabhrj31@gmail.com>
2020-04-30 16:51:31 +0530

    diameter of a tree

---
+++ dia.cpp
@@ -0,0 +1,18 @@
+
+vector<int> par;
+pair<int,int> dfs(int u,int p =-1,int d=0){
+	pair<int,int> res = make_pair(d,u);
+	for(auto child:adj[u]){
+		if(child==p)continue;
+		par[child] = u;
+		res = max(res,dfs(v,u,d+1));
+	}
+	return res;
+}
+
+int main(){
+	//input for the graph ;
+	pair<int,int> da = dfs(0);
+	pair<int,int> db= dfs(da,second);
+	//da and db are the end points of the diameter
+}
